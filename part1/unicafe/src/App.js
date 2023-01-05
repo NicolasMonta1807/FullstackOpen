65b2712a343db9e738ce05e98fcda39b9b6d48b0
Nicolás Montañez Velasco <edge32195@outlook.es>
2023-01-05 15:04:50 -0500

    1.7 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,27 +7,45 @@ const FeedbackButton = ({ text, handleClick }) => (
 )
 
 const Statistics = ({ values }) => {
-  console.log(values);
+
+  const allFeedbacks = values.good + values.neutral + values.bad
+
+  const calculateAverage = () => ((values.good - values.bad) / allFeedbacks)
+
+  const calculatePositive = () => ((values.good * 100) / allFeedbacks)
+
   return (
     <>
       <h2>Statistics</h2>
-      <p>Good: {values[0]}</p>
-      <p>Neutral: {values[1]}</p>
-      <p>Bad: {values[2]}</p>
+      <p>Good: {values.good}</p>
+      <p>Neutral: {values.neutral}</p>
+      <p>Bad: {values.bad}</p>
+      <h3>Totals:</h3>
+      <p>All: {allFeedbacks}</p>
+      <p>Average: {calculateAverage()}</p>
+      <p>Positive: {calculatePositive()}%</p>
     </>
   )
 }
 
 function App() {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [feedbacks, setFeedbacks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
 
-  const handleGoodClick = () => { setGood(good + 1) }
+  const handleGoodClick = () => {
+    setFeedbacks({ ...feedbacks, good: feedbacks.good + 1 })
+  }
 
-  const handleNeutralClick = () => { setNeutral(neutral + 1) }
+  const handleNeutralClick = () => {
+    setFeedbacks({ ...feedbacks, neutral: feedbacks.neutral + 1 })
+  }
 
-  const handleBadClick = () => { setBad(bad + 1) }
+  const handleBadClick = () => {
+    setFeedbacks({ ...feedbacks, bad: feedbacks.bad + 1 })
+  }
 
   return (
     <>
@@ -35,7 +53,7 @@ function App() {
       <FeedbackButton text="Good" handleClick={handleGoodClick} />
       <FeedbackButton text="Neutral" handleClick={handleNeutralClick} />
       <FeedbackButton text="Bad" handleClick={handleBadClick} />
-      <Statistics values={[good, neutral, bad]} />
+      <Statistics values={feedbacks} />
     </>
   );
 }
