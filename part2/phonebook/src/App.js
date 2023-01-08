106a6f64d898a41133190c95269e1345b0e82e6f
Nicolás Montañez Velasco <edge32195@outlook.es>
2023-01-08 13:46:28 -0500

    2.8 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,22 +5,31 @@ const App = () => {
 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const handleNameChange = (event) => setNewName(event.target.value)
 
   const handleNewContact = (event) => {
-
     event.preventDefault()
-    if (persons.includes(newName)) {
+    const alreadyAdded = persons.some(person => person.name === newName)
+    if (alreadyAdded) {
       alert(`${newName} is already added to phonebook`)
       setNewName("")
+      setNewNumber("")
     }
     else {
-      setPersons(persons.concat(newName))
+      const personObject = {
+        id: persons.length + 1,
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(personObject))
       setNewName("")
+      setNewNumber("")
     }
-
   }
+
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   return (
     <>
@@ -31,12 +40,20 @@ const App = () => {
           <input id='name' type="text" value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          <label htmlFor="phonenumber">Number:</label>
+          <input id='phonenumber' type="number" value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit" onClick={handleNewContact}>Save contact</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => <p key={person} >{person}</p>)}
+        {persons.map((person) => (
+          <div key={person}>
+            <p>{person.name} - {person.number}</p>
+          </div>
+        ))}
       </div>
     </>
   );
