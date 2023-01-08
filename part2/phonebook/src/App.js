c3a499fc3de18e64370fd88bbcb07400744fc74a
Nicolás Montañez Velasco <edge32195@outlook.es>
2023-01-08 14:22:36 -0500

    2.9 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,16 @@
-import { eventWrapper } from '@testing-library/user-event/dist/utils';
 import { useState } from 'react';
 
 const App = () => {
 
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+  const [filter, setFilter] = useState("")
 
   const handleNameChange = (event) => setNewName(event.target.value)
 
@@ -31,9 +36,23 @@ const App = () => {
 
   const handleNumberChange = (event) => setNewNumber(event.target.value)
 
+  const handleFilter = (event) => setFilter(event.target.value)
+
+  const contactsToShow = () => {
+    const showingContacts = persons.filter((person) => (
+      person.name.toLowerCase().includes(filter.toLowerCase())
+    ))
+    return showingContacts
+  }
+
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        <label htmlFor="filter">Search by name: </label>
+        <input type="text" value={filter} onChange={handleFilter} />
+      </div>
+      <h2>Add new contact</h2>
       <form>
         <div>
           <label htmlFor="name">Name:</label>
@@ -49,10 +68,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
-          <div key={person}>
-            <p>{person.name} - {person.number}</p>
-          </div>
+        {contactsToShow().map((person) => (
+          <p key={person.id}>{person.name} - {person.number}</p>
         ))}
       </div>
     </>
