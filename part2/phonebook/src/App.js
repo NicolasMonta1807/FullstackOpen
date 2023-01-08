77f15eaa9c06939736e149f65633d2e23439b41e
Nicolás Montañez Velasco <edge32195@outlook.es>
2023-01-08 13:34:55 -0500

    2.6 completed

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,37 @@
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
+import { useState } from 'react';
+
+const App = () => {
+
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState("")
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+
+  const handleNewContact = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat(newName))
+    setNewName("")
+  }
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input id='name' type="text" value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleNewContact}>Save contact</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => <p key={person} >{person}</p>)}
+      </div>
+    </>
+  );
+}
+
+export default App;
