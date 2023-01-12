d9c02bd01b43e1996455f4d485b2c0bd9503efb4
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2023-01-12 16:15:13 -0500

    3.3 completed

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -40,5 +40,15 @@ app.get('/api/persons', (request, response) => {
   response.status(200).json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  if (!person) {
+    return response.status(404).json({ error: 'Resource not found' })
+  }
+  return response.status(200).json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
