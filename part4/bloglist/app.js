5e7d0b6ac9c9239f987d203325652ea9f791d10b
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2023-02-14 08:47:27 -0500

    Using standard linter

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,17 +1,17 @@
-const config = require("./utils/config")
-const mongoose = require("mongoose")
-const express = require("express")
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const express = require('express')
 const cors = require('cors')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const app = express()
-const blogRouter = require("./controllers/blogs")
+const blogRouter = require('./controllers/blogs')
 
-mongoose.set("strictQuery", false)
+mongoose.set('strictQuery', false)
 
 mongoose
   .connect(config.MONGODB_URI)
-  .then(() => logger.info("Connected to MongoDB"))
+  .then(() => logger.info('Connected to MongoDB'))
   .catch((error) => logger.error(error.message))
 
 app.use(cors())
